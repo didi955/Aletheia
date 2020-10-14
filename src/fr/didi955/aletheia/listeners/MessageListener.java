4ee4b9ee6eda17443e25c484908f2356e1b28005
rushc <lannuzeld@gmail.com>
2020-10-14 22:43:11 +0200

    Minors Bug fix

--- src/fr/didi955/aletheia/listeners/MessageListener.java
+++ src/fr/didi955/aletheia/listeners/MessageListener.java
@@ -1,5 +1,6 @@
 package fr.didi955.aletheia.listeners;
 
+import fr.didi955.aletheia.Aletheia;
 import fr.didi955.aletheia.commands.CommandMap;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -18,6 +19,7 @@ public class MessageListener {
         }
         String message = event.getMessage().getContentRaw();
         if(message.startsWith(CommandMap.getTag())){
+            if(event.getTextChannel() != null && !event.getTextChannel().getId().equals(Aletheia.CONFIGURATION.getString("channelCommandsID", "id"))) return;
             message = message.replaceFirst(CommandMap.getTag(), "");
             if(commandMap.commandUser(event.getAuthor(), message, event.getMessage())){
                 if(event.getTextChannel() != null && event.getGuild().getSelfMember().hasPermission(Permission.MESSAGE_MANAGE)){
